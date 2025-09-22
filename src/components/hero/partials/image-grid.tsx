1b63f0cb28c22fd55a5cc6c1a825e15e9a9f1f45
Nonso Anetoh <anet0001@algonquinlive.com>
2025-09-21 20:49:45 -0700

    add: base image grid component

---
+++ src/components/hero/partials/image-grid.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+interface ImageGridProps {
+  images: string[];
+}
+
+const ImageGrid: FC<ImageGridProps> = ({ images }) => {
+  return <div>ImageGrid</div>;
+};
+
+export default ImageGrid;
