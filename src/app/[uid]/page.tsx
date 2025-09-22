8facc6f3685076d0651e574ceb35b83b788175c8
Nonso Anetoh <anet0001@algonquinlive.com>
2025-09-21 20:50:03 -0700

    update: root div style to include uid

---
+++ src/app/[uid]/page.tsx
@@ -0,0 +1,54 @@
+import * as prismic from "@prismicio/client";
+import { SliceZone } from "@prismicio/react";
+import { notFound } from "next/navigation";
+
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { uid: string };
+}) {
+  const { uid } = await params;
+  const client = createClient();
+  const page = await client.getByUID("page", uid).catch(() => notFound());
+
+  return {
+    title: `${prismic.asText(page.data.title)}`,
+    description: page.data.meta_description,
+    openGraph: {
+      title: page.data.meta_title ?? "",
+      images: [
+        {
+          url: page.data.meta_image?.url ?? "",
+        },
+      ],
+    },
+  };
+}
+
+export default async function Page({ params }: { params: { uid: string } }) {
+  const { uid } = params;
+  const client = createClient();
+
+  const page = await client.getByUID("page", uid).catch(() => notFound());
+
+  return (
+    <div className={`page page--${uid}`}>
+      <div className="inner">
+        <SliceZone slices={page.data.slices} components={components} />
+      </div>
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  const client = createClient();
+
+  const pages = await client.getAllByType("page");
+
+  return pages.map((page) => {
+    return { uid: page.uid };
+  });
+}
