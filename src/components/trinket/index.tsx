bd5472c6a51ed63d20cbdeba5702e7d33000bbf1
Nonso Anetoh <anet0001@algonquinlive.com>
2025-10-13 01:51:41 -0700

    update: trinket component

--- src/components/trinket/index.tsx
+++ src/components/trinket/index.tsx
@@ -1,30 +1,14 @@
 import React, { FC } from "react";
 import SVG from "./partials/svg";
-import { parseTrinkets } from "@/utils/trinkets";
 
-export type TrinketComponentProps = ReturnType<typeof parseTrinkets> & {
-  use?: string;
+export type TrinketComponentProps = {
+  id: string;
+  name: string;
+  type: "static" | "interactive";
 };
 
-const Trinket: FC<TrinketComponentProps> = ({
-  id,
-  name,
-  style,
-  type,
-  callback,
-  size,
-}) => {
-  return (
-    <SVG
-      id={id}
-      key={id}
-      name={name}
-      style={style}
-      type={type}
-      callback={callback}
-      size={size}
-    />
-  );
+const Trinket: FC<TrinketComponentProps> = ({ id, name, type }) => {
+  return <SVG id={id} name={name} type={type} />;
 };
 
 export default Trinket;
